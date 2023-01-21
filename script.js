a664a7eaecbe5e4d42af81ccfeaa8280a78d134b
David Bomfim <davidrochabomfim6@gmail.com>
2023-01-21 19:50:02 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Esse dia já está incluso, camarada!🙅‍♀️❌")
+    return
+  }
+  alert("Adicionado com sucesso,chefe🤙✅")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
